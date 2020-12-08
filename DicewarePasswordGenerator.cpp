a7b9012673cb9a98c275a4c0ab45f6c7e1ad4d5e
Vishwajeet-Singh39 <54769984+Vishwajeet-Singh39@users.noreply.github.com>
2020-12-09 04:33:57 +0530

    Bug fixes

--- DicewarePasswordGenerator.cpp
+++ DicewarePasswordGenerator.cpp
@@ -8,21 +8,23 @@ void randomNumber(int dice[]){
 	uniform_int_distribution<int> dis(1,6);
 	for(int i=0;i<5;i++){
 		dice[i]=dis(gen);
+		cout<<dice[i]<<" ";
 	}
+	cout<<endl;
 }
 int getIndex(int dice[]){
     int index;
-    int value1[]={0 , 1297, 2593, 3889, 5185, 6481};
+    int value1[]={0 , 1296, 2592, 3888, 5184, 6480};
     int value2[]={0 , 216, 432, 648, 864, 1080};
-    int value3[]={0, 37, 73, 109, 145, 181};
-    int value4[]={0, 7, 13, 19, 25, 31};
+    int value3[]={0, 36, 72, 108, 144, 180};
+    int value4[]={0, 6, 12, 18, 24, 30};
     int value5[]={0, 1, 2, 3, 4, 5};
 
-    index= value1[dice[1]-1];
-    index+=value2[dice[2]-1];
-    index+=value3[dice[3]-1];
-    index+=value4[dice[4]-1];
-    index+=value5[dice[5]-1];
+    index= value1[dice[0]-1];
+    index+=value2[dice[1]-1];
+    index+=value3[dice[2]-1];
+    index+=value4[dice[3]-1];
+    index+=value5[dice[4]-1];
     return index;
 }
 string generatePhrase(){

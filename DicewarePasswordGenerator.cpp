52b113b42a5b9f62dafaf1c9e7621da01af17177
Vishwajeet-Singh39 <54769984+Vishwajeet-Singh39@users.noreply.github.com>
2020-12-08 20:38:14 +0530

    Create DicewarePasswordGenerator.cpp
    Random number genration from 1 to 5

---
+++ DicewarePasswordGenerator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+void randomNumber(){
+    srand(time(0));
+    for(int i=0;i<5;i++){
+        cout<<(rand()%5)+1;   
+    }
+}
+int main(){
+    randomNumber();
+}
